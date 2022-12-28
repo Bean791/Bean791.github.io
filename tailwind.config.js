df4a710408e7d43111e4a556350bf2f6f504edd7
Bean791 <bean90917@gmail.com>
2022-12-28 16:11:44 +0700

    oke web mode taildwin suda kelar untuk keraknya dan sudah responsif di hp cuma untuk darkmode masih menyusul dan rencana data akan di pindah ke satu js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ['tailwind.html'],
+  theme: {
+    extend: {
+      colors: {
+        'satu':'#363636',
+        'dua':'#DC2F2F',
+        'tiga':'#FF894C',
+        'empat':'#F8F8F8'
+      },
+    },
+  },
+  plugins: [],
+}
